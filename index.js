1b7c6dfa260b035356a8cd04fac19a5536df146d
jordan-laird <jordanmack.laird@gmail.com>
2018-11-19 17:40:44 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+// const employees = ['Ada', 'Brendan', 'Ali']
+function printBadges(employees){
+    for ( let i = 0; i < employees.length; i++){
+        console.log(`Welcome ${employees[i]}! You are employee #${i + 1}.`);
+    }
+    return employees;
+}
+
+function tailsNeverFails(){
+    let coin = Math.round(Math.random());
+    i = 0
+    function result() {
+        return `You got ${i} tails in a row!`
+    }
+    while (coin === 1){
+        i ++
+        coin = Math.round(Math.random());
+    };
+    return result();
+}
