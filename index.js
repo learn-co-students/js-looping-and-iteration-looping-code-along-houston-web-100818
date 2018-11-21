9d20584f9f236f301c7a1da19898523c704e3d77
Robert Schulte <schulte.robertw@outlook.com>
2018-11-20 23:06:18 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function printBadges(names) {
+  for (let i = 0; i < names.length; i++) {
+    console.log(`Welcome ${names[i]}! You are employee #${i + 1}.`);
+  }
+  return names;
+}
+
+function tailsNeverFails() {
+  let count = 0;
+  let result = true;
+  while (result) {
+    if (Math.random() >= 0.5) {
+      count++;
+    } else {
+      result = false;
+    }
+  }
+  return `You got ${count} tails in a row!`
+}
